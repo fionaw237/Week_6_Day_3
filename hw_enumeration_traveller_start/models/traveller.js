78ee56c2fd8e251b0497178d63dd5bd7128da262
Fiona Wilson <fionaw237@gmail.com>
2018-08-22 18:13:41 +0100

    Adds initial solutions

--- hw_enumeration_traveller_start/models/traveller.js
+++ hw_enumeration_traveller_start/models/traveller.js
@@ -3,31 +3,49 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  result = this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+  return result;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  result = this.journeys.map((journey) => {
+    return journey.endLocation
+  });
+  return result;
 };
 
 Traveller.prototype.getModesOfTransport = function () {
-
+  result = this.journeys.map((journey) => {
+      return journey.transport
+  });
+  return result;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  result = this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+  return result;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  result = this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  });
+  return result;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  result = this.journeys.reduce((total, journey) => {
+    return total + journey.distance;
+  }, 0);
+  return result;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  
+
 };
 
 
