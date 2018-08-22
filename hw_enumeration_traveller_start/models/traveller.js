21569e60bd0f4f8201817038e7f3a5b4716b631b
Fiona Wilson <fionaw237@gmail.com>
2018-08-22 19:26:50 +0100

    Adds possible solution to last part

--- hw_enumeration_traveller_start/models/traveller.js
+++ hw_enumeration_traveller_start/models/traveller.js
@@ -45,8 +45,14 @@ Traveller.prototype.calculateTotalDistanceTravelled = function () {
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  modesOfTransports = this.getModesOfTransport()
+  result = modesOfTransports.filter((transport, index, self) => {
+    return self.indexOf(transport) === index;
+  // this works because self.indexOf teturns the first index in self (modesOfTransports) at which transport occurs
+  });
+  return result;
 };
+
 
 
 module.exports = Traveller;
